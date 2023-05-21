d4458dab7a20e0ed68ecb46eea9cbad7f01c3da6
Artur Hostiuk <ahostiuk@solvd.com>
2023-05-21 12:39:12 +0300

    Add select date range for predict

--- djangoproject/frontend/src/components/screens/InternalMigration.tsx
+++ djangoproject/frontend/src/components/screens/InternalMigration.tsx
@@ -10,25 +10,55 @@ import {Legend} from "../ui/map/Legend.tsx";
 import {MigrationInterface} from "../../types/migration.interface.ts";
 import {MigrationService} from "../../services/migration.service.ts";
 import {DataScopeSelector} from "../ui/map/DataScopeSelector.tsx";
+// @ts-ignore
+import Datepicker from "tailwind-datepicker-react"
 
 
 const dataScopes: DataScope[] = [
     {
-        name: "Population",
-        key: "pop_est",
+        name: "Migration",
+        key: "Migration",
         description: "The population of the oblast",
         unit: "",
         // scale: [5000000, 10000000, 25000000, 50000000, 75000000, 100000000, 200000000, 1000000000]
         scale: [1000, 10000, 25000, 50000, 100000, 200000, 500000, 1000000]
     },
     {
-        name: "GDP",
-        key: "gdp_md_est",
+        name: "Immigration",
+        key: "Immigration",
         description: "The GDP of the country",
-        unit: "USD",
-        scale: [100000, 250000, 500000, 1000000, 2500000, 5000000, 15000000]
+        unit: "",
+        scale: [10000, 25000, 50000, 100000, 250000, 500000, 1500000]
     }
 ];
+
+const options = {
+    // title: "Demo Title",
+    autoHide: true,
+    todayBtn: false,
+    clearBtn: true,
+    maxDate: new Date("2030-01-01"),
+    minDate: new Date("1950-01-01"),
+    theme: {
+        background: "bg-gray-700 dark:bg-gray-800",
+        todayBtn: "",
+        clearBtn: "",
+        icons: "",
+        text: "",
+        disabledText: "bg-red-500",
+        input: "",
+        inputIcon: "",
+        selected: "",
+    },
+    icons: {
+        // () => ReactElement | JSX.Element
+        prev: () => <span>Previous</span>,
+        next: () => <span>Next</span>,
+    },
+    datepickerClassNames: "top-12 z-10",
+    defaultDate: new Date("2023-01-01"),
+    language: "en",
+}
 
 const colors = [
     ['#fcfca7', '#f4e283', '#eec762', '#e8ab44', '#e28d2b', '#dc6e16', '#d4490a', '#cb0c0c'],
@@ -42,6 +72,32 @@ const InternalMigration = () => {
     const [migrations, setMigrations] = useState<MigrationInterface[]>([])
 
     const geoMap = useRef<GeoJson>(null);
+
+    const [fromShow, setFromShow] = useState<boolean>(false)
+    const [fromDate, setFromDate] = useState<Date>(new Date("2023-01-01"))
+    const [toShow, setToShow] = useState<boolean>(false)
+    const [toDate, setToDate] = useState<Date>(new Date("2024-01-01"))
+    const [toOptions, setToOptions] = useState({...options, minDate: new Date("2023-01-01"), defaultDate: new Date("2024-01-01")})
+    const handleFromChange = (selectedDate: Date) => {
+        setToOptions(prevState => {
+            return {...prevState, minDate: new Date(selectedDate), defaultDate: new Date(selectedDate)}
+        })
+        setFromDate(selectedDate)
+    }
+    const handleFromClose = (state: boolean) => {
+        setFromShow(state)
+    }
+    const handleToChange = (selectedDate: Date) => {
+        setToDate(selectedDate)
+    }
+    const handleToClose = (state: boolean) => {
+        setToShow(state)
+    }
+
+    const predictMigration = async () => {
+        const data = await MigrationService.getInternalMigration(fromDate, toDate)
+        setMigrations(data)
+    }
 
     useEffect(() => {
 
@@ -68,14 +124,15 @@ const InternalMigration = () => {
                     const migration = migrations?.filter(e => e.Oblast == name).at(0)
                     let migrationValue = 0
                     if(migration) {
-                        migrationValue = migration.Migration
+                        //@ts-ignore
+                        migrationValue = migration[dataScope.key]
                     }
-                    l.setTooltipContent(`<div><span>Migration</span>: ${Math.ceil(migrationValue)}</div>`)
+                    l.setTooltipContent(`<div><span>${dataScope.key}</span>: ${Math.ceil(migrationValue)}</div>`)
                 }
             })
         }
 
-    }, [migrations])
+    }, [dataScope, migrations])
 
     const handleDataScopeChange = (event: ChangeEvent<HTMLSelectElement>) => {
         const scope = dataScopes.find(element => element.key === event.target.value)
@@ -129,7 +186,9 @@ const InternalMigration = () => {
         const migration = migrations?.filter(e => e.Oblast == name).at(0)
         let migrationValue = 0
         if(migration) {
-            migrationValue = migration.Migration
+            //@ts-ignore
+            migrationValue = migration[dataScope.key]
+            // console.log(`for ${dataScope.key}: `, migrationValue)
         }
 
         const mapStyle = {
@@ -152,27 +211,39 @@ const InternalMigration = () => {
     );
 
     return(
-        <div className="mapContainer">
-            <MapContainer center={[48.5, 31.5]}
-                          zoomControl={false}
-                          zoom={6}
-                          maxZoom={8}
-                          minZoom={2}
-                          zoomSnap={0.5}
-                          zoomDelta={0.5}
-                          wheelPxPerZoomLevel={120}
-                          maxBoundsViscosity={0.5}
-                          maxBounds={L.latLngBounds(new L.LatLng(85, -210), new L.LatLng(-85, 210))}>
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                {/*<TileLayer url={`https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png?apikey=${import.meta.env.VITE_MAP_API_KEY}`} />*/}
-                <LayersControl position="topleft">
-                    <LayersControl.Overlay checked name="GeoJSON">
-                        {geoJsonComponent}
-                    </LayersControl.Overlay>
-                </LayersControl>
-                <Legend scope={dataScope} colors={colors} hoveredCountry={hoveredCountry} migrations={migrations}/>
-            </MapContainer>
-            <DataScopeSelector options={dataScopes} value={dataScope} changeHandler={handleDataScopeChange} />
+        <div className="h-full flex flex-col">
+            <div className="flex mb-4">
+                <DataScopeSelector options={dataScopes} value={dataScope} changeHandler={handleDataScopeChange} />
+                <div className="flex items-center ml-4">
+                    <Datepicker options={options} onChange={handleFromChange} show={fromShow} setShow={handleFromClose} />
+                    <span className="mx-4 text-gray-500">to</span>
+                    <Datepicker options={toOptions} onChange={handleToChange} show={toShow} setShow={handleToClose} />
+                    <button onClick={predictMigration}
+                            className="bg-gray-50 ml-4 w-min border hover:bg-gray-100 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    >Predict</button>
+                </div>
+            </div>
+            <div className="mapContainer z-0">
+                <MapContainer center={[48.5, 31.5]}
+                              zoomControl={false}
+                              zoom={6}
+                              maxZoom={8}
+                              minZoom={2}
+                              zoomSnap={0.5}
+                              zoomDelta={0.5}
+                              wheelPxPerZoomLevel={120}
+                              maxBoundsViscosity={0.5}
+                              maxBounds={L.latLngBounds(new L.LatLng(85, -210), new L.LatLng(-85, 210))}>
+                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                    {/*<TileLayer url={`https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png?apikey=${import.meta.env.VITE_MAP_API_KEY}`} />*/}
+                    <LayersControl position="topleft">
+                        <LayersControl.Overlay checked name="GeoJSON">
+                            {geoJsonComponent}
+                        </LayersControl.Overlay>
+                    </LayersControl>
+                    <Legend scope={dataScope} colors={colors} hoveredCountry={hoveredCountry} migrations={migrations}/>
+                </MapContainer>
+            </div>
         </div>
     )
 }
