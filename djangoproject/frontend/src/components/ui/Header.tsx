4810bbba7b620fb775a83be234ac3e841f09e32d
Artur Hostiuk <ahostiuk@solvd.com>
2023-05-22 09:42:17 +0300

    Add register screen

--- djangoproject/frontend/src/components/ui/Header.tsx
+++ djangoproject/frontend/src/components/ui/Header.tsx
@@ -35,7 +35,7 @@ const Header = () => {
                                     <Link to="/login"
                                        className="text-gray-800 dark:text-white hover:bg-gray-200 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log
                                         in</Link>
-                                    <Link to="/singup"
+                                    <Link to="/register"
                                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Sign
                                         up</Link>
                                 </>
--- djangoproject/frontend/src/routes/Router.tsx
+++ djangoproject/frontend/src/routes/Router.tsx
@@ -2,11 +2,13 @@ import {Route, Routes} from "react-router-dom";
 import Home from "../components/screens/Home.tsx";
 import Login from "../components/screens/Login.tsx";
 import InternalMigration from "../components/screens/InternalMigration.tsx";
+import Register from "../components/screens/Register.tsx";
 
 const Router = () => {
     return(
         <Routes>
             <Route path='/' element={<Home />}/>
+            <Route path='/register' element={<Register />}/>
             <Route path='/login' element={<Login />}/>
             <Route path='/migration/internal' element={<InternalMigration />}/>
         </Routes>
